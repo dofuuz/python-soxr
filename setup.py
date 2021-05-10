7f53c71ed3b8d78d6c030b34996d1c78eef3cde9
dofuuz <dofu@nate.com>
2021-05-10 22:54:51 +0900

    Exclude CR64S for better compatibility

--- setup.py
+++ setup.py
@@ -27,16 +27,28 @@ src = [
     'libsoxr/src/filter.c',
     'libsoxr/src/fft4g64.c',
     'libsoxr/src/cr.c',
+
+    # WITH_CR32
     'libsoxr/src/cr32.c',
     'libsoxr/src/fft4g32.c',
+
+    # WITH_CR64
     'libsoxr/src/cr64.c',
-    'libsoxr/src/vr32.c',
+
+    # WITH_VR32
+    # 'libsoxr/src/vr32.c',
+
+    # WITH_CR32S
     'libsoxr/src/cr32s.c',
     'libsoxr/src/pffft32s.c',
     'libsoxr/src/util32s.c',
-    'libsoxr/src/cr64s.c',
-    'libsoxr/src/pffft64s.c',
-    'libsoxr/src/util64s.c',
+
+    # WITH_CR64S
+    # 'libsoxr/src/cr64s.c',
+    # 'libsoxr/src/pffft64s.c',
+    # 'libsoxr/src/util64s.c',
+
+    # Cython wrapper
     'csoxr.pxd',
     'cysoxr.pyx'
 ]
@@ -45,31 +57,41 @@ extensions = [
     CySoxrExtension(
         "cysoxr",
         src,
-        include_dirs=['libsoxr/src', 'libsoxr/msvc'],
+        include_dirs=['libsoxr/src', 'soxr'],
         language="c",
-        extra_compile_args=['-DSOXR_LIB', '-march=native'])
+        extra_compile_args=['-DSOXR_LIB'])
 ]
+
 setup(
     name="soxr",
     version="0.0.3",
     author="dofuuz",
-    description="Python Wrapper for libsoxr",
+    description="High quality, one-dimensional sample-rate conversion library",
+    keywords='samplerate, SRC',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dofuuz/python-soxr",
     packages=find_packages(),
-    package_data={'soxr':["*.pyx", "*.h"]},
     ext_package='soxr',
     ext_modules=extensions,
-    python_requires='>=3.5'
+    python_requires='>=3.5',
     install_requires=['numpy'],
     tests_require=['pytest'],
     classifiers=[
         "Development Status :: 3 - Alpha",
+
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Telecommunications Industry",
+        "Topic :: Multimedia :: Sound/Audio :: Analysis",
+        "Topic :: Multimedia :: Sound/Audio :: Conversion",
+        "Topic :: Scientific/Engineering",
+
+        "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
+
         "Programming Language :: C",
         "Programming Language :: Python",
-        "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
-        "Operating System :: OS Independent",
     ],
     license_files=['LICENSE.txt'],
 )
