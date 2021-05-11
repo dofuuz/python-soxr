19b354b247e15de2411d1a20226e8a588c444c7c
dofuuz <dofu@nate.com>
2021-05-11 23:21:13 +0900

    Use Memoryview instead of np.ndarray

--- cysoxr.pyx
+++ cysoxr.pyx
@@ -1,10 +1,36 @@
 # Python wrapper for libsoxr
 # https://github.com/dofuuz/python-soxr
 
+cimport cython
+
 import numpy as np
 cimport numpy as np
 
 cimport csoxr
+
+
+ctypedef fused datatype_t:
+    cython.float
+    cython.double
+    cython.int
+    cython.short
+    
+
+# NumPy scalar type to soxr_io_spec_t
+cdef csoxr.soxr_io_spec_t to_io_spec(type dtype):
+    cdef csoxr.soxr_datatype_t io_type
+    if dtype == np.float32:
+        io_type = csoxr.SOXR_FLOAT32_I
+    elif dtype == np.float64:
+        io_type = csoxr.SOXR_FLOAT64_I
+    elif dtype == np.int32:
+        io_type = csoxr.SOXR_INT32_I
+    elif dtype == np.int16:
+        io_type = csoxr.SOXR_INT16_I
+    else:
+        raise ValueError('Data type not support')
+    
+    return csoxr.soxr_io_spec(io_type, io_type)
 
 
 cdef class CySoxr:
@@ -18,19 +44,7 @@ cdef class CySoxr:
         self._out_rate = out_rate
         self._dtype = dtype
 
-        cdef csoxr.soxr_datatype_t io_type
-        if dtype == np.float32:
-            io_type = csoxr.SOXR_FLOAT32_I
-        elif dtype == np.float64:
-            io_type = csoxr.SOXR_FLOAT64_I
-        elif dtype == np.int32:
-            io_type = csoxr.SOXR_INT32_I
-        elif dtype == np.int16:
-            io_type = csoxr.SOXR_INT16_I
-        else:
-            raise ValueError('Dtype not support')
-
-        cdef csoxr.soxr_io_spec_t io_spec = csoxr.soxr_io_spec(io_type, io_type)
+        cdef csoxr.soxr_io_spec_t io_spec = to_io_spec(dtype)
 
         self._soxr = csoxr.soxr_create(in_rate, out_rate, num_channels, NULL, &io_spec, NULL, NULL)
         if self._soxr is NULL:
@@ -39,32 +53,37 @@ cdef class CySoxr:
     def __dealloc__(self):
         csoxr.soxr_delete(self._soxr)
 
-    cpdef np.ndarray process(self, np.ndarray x, bint eof=False):
-        if 2 < x.ndim:
-            raise ValueError('Input must be 1-D or 2-D array')
-
+    cpdef datatype_t[:, ::1] process(self, datatype_t[:, ::1] x, bint eof=False):
         cdef size_t ilen = x.shape[0]
         cdef size_t olen = np.ceil(ilen * self._out_rate / self._in_rate)
-        cdef unsigned channels = 1
-        if 2 == x.ndim:
-            channels = x.shape[1]
+        cdef unsigned channels = x.shape[1]
 
-        dtype = x.dtype
+        # Cython type to NumPy scalar type
+        cdef type dtype
+        if datatype_t is cython.float:
+            dtype = np.float32
+        elif datatype_t is cython.double:
+            dtype = np.float64
+        elif datatype_t is cython.int:
+            dtype = np.int32
+        elif datatype_t is cython.short:
+            dtype = np.int16
+
         if dtype != self._dtype:
-            raise ValueError('Dtype not match')
+            raise ValueError('Data type not match')
 
-        x = np.ascontiguousarray(x)
-        cdef np.ndarray out_buf = np.zeros([olen, channels], dtype=dtype, order='c')
+        # x = np.ascontiguousarray(x)
+        cdef datatype_t[:, ::1] out_buf = np.zeros([olen, channels], dtype=dtype, order='c')
         cdef size_t odone
 
         csoxr.soxr_process(
             self._soxr,
-            x.data, ilen, NULL,
-            out_buf.data, olen, &odone)
+            &x[0,0], ilen, NULL,
+            &out_buf[0,0], olen, &odone)
 
         out_buf = out_buf[:odone]
 
-        cdef np.ndarray eof_buf
+        cdef datatype_t[:, ::1] eof_buf
         cdef int delay
         if (eof):
             delay = int(csoxr.soxr_delay(self._soxr) + .5)
@@ -72,7 +91,7 @@ cdef class CySoxr:
             csoxr.soxr_process(
                 self._soxr,
                 NULL, 0, NULL,
-                eof_buf.data, delay, &odone)
+                &eof_buf[0,0], delay, &odone)
 
             eof_buf = eof_buf[:odone]
 
@@ -80,45 +99,36 @@ cdef class CySoxr:
 
         return out_buf
 
-        # return np.copy(self._out_buf[:odone])
 
-
-cpdef np.ndarray cysoxr_oneshot(double in_rate, double out_rate, np.ndarray x):
-    if 2 < x.ndim:
-        raise ValueError('Input must be 1-D or 2-D array')
-
+cpdef datatype_t[:, ::1] cysoxr_oneshot(double in_rate, double out_rate, datatype_t[:, ::1] x):
     cdef size_t ilen = x.shape[0]
     cdef size_t olen = np.ceil(ilen * out_rate / in_rate)
-    cdef unsigned channels = 1
-    if 2 == x.ndim:
-        channels = x.shape[1]
+    cdef unsigned channels = x.shape[1]
 
-    dtype = x.dtype
+    # Cython type to NumPy scalar type
+    cdef type dtype
+    if datatype_t is cython.float:
+        dtype = np.float32
+    elif datatype_t is cython.double:
+        dtype = np.float64
+    elif datatype_t is cython.int:
+        dtype = np.int32
+    elif datatype_t is cython.short:
+        dtype = np.int16
 
-    cdef csoxr.soxr_datatype_t io_type
-    if dtype == np.float32:
-        io_type = csoxr.SOXR_FLOAT32_I
-    elif dtype == np.float64:
-        io_type = csoxr.SOXR_FLOAT64_I
-    elif dtype == np.int32:
-        io_type = csoxr.SOXR_INT32_I
-    elif dtype == np.int16:
-        io_type = csoxr.SOXR_INT16_I
-    else:
-        raise ValueError('Dtype not support')
-
-    cdef csoxr.soxr_io_spec_t io_spec = csoxr.soxr_io_spec(io_type, io_type)
+    # make soxr config
+    cdef csoxr.soxr_io_spec_t io_spec = to_io_spec(dtype)
     cdef csoxr.soxr_quality_spec_t quality = csoxr.soxr_quality_spec(csoxr.SOXR_HQ, 0)
 
-    x = np.ascontiguousarray(x)    # make array C-contiguous
+    # x = np.ascontiguousarray(x)    # make array C-contiguous
 
     cdef size_t odone
-    cdef np.ndarray out_ndarray = np.zeros([olen, channels], dtype=dtype, order='c')
+    cdef datatype_t[:, ::1] y = np.zeros([olen, channels], dtype=dtype, order='c')
 
     csoxr.soxr_oneshot(
         in_rate, out_rate, channels,
-        x.data, ilen, NULL,
-        out_ndarray.data, olen, &odone,
+        &x[0,0], ilen, NULL,
+        &y[0,0], olen, &odone,
         &io_spec, &quality, NULL)
 
-    return out_ndarray[:odone]
+    return y[:odone]
--- soxr/__init__.py
+++ soxr/__init__.py
@@ -22,6 +22,11 @@ class ResampleStream():
         if type(x) != np.ndarray or x.dtype.type != self._type:
             x = np.asarray(x, dtype=self._type)
 
+        if x.ndim == 1:
+            x = np.expand_dims(x, axis=1)
+            y = self._cysoxr.process(x, end)
+            return np.squeeze(y, axis=1)
+
         return self._cysoxr.process(x, end)
 
 
@@ -31,5 +36,10 @@ def resample(x, in_rate: float, out_rate: float):
 
     if not x.dtype.type in (np.float32, np.float64, np.int16, np.int32):
         raise ValueError("dtype must be one of ['float32', 'float64', 'int16', 'int32'] and not {}".format(x.dtype.type))
+
+    if x.ndim == 1:
+        x = np.expand_dims(x, axis=1)
+        y = cysoxr_oneshot(in_rate, out_rate, x)
+        return np.squeeze(y, axis=1)
 
     return cysoxr_oneshot(in_rate, out_rate, x)
